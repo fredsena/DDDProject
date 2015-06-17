df94e1f208cc9ec75962991fdac42371d8b3fbfc
fredsena <sena.fred@gmail.com>
2015-06-17 19:29:08 -0300

    Included xUnit test project

---
+++ DDDProject.UnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace DDDProject.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.False(false);
+        }
+    }
+}
