a1b97f198cad925539a578c9881a6d3596700dcb
fredsena <sena.fred@gmail.com>
2015-06-18 16:35:14 -0300

    work in progress

---
+++ DDDProject.Data.Context/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDProject.Data.Context.Interfaces
+{
+    interface IUnitOfWork
+    {
+    }
+}
---
+++ DDDProject.Data.Context/northwindContext.cs
@@ -0,0 +1,54 @@
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using DDDProject.Data.Context.Mapping;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using DDDProject.Domain.Entities;
+
+namespace DDDProject.Data.Context
+{
+    public class northwindContext : DbContext
+    {
+        static northwindContext()
+        {
+            Database.SetInitializer<northwindContext>(null);
+        }
+
+        public northwindContext()
+            : base("Name=northwindContext")
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<CustomerDemographic> CustomerDemographics { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Order_Detail> Order_Details { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Shipper> Shippers { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Territory> Territories { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Configurations.Add(new CategoryMap());
+            modelBuilder.Configurations.Add(new ContactMap());
+            modelBuilder.Configurations.Add(new CustomerDemographicMap());
+            modelBuilder.Configurations.Add(new CustomerMap());
+            modelBuilder.Configurations.Add(new EmployeeMap());
+            modelBuilder.Configurations.Add(new Order_DetailMap());
+            modelBuilder.Configurations.Add(new OrderMap());
+            modelBuilder.Configurations.Add(new ProductMap());
+            modelBuilder.Configurations.Add(new RegionMap());
+            modelBuilder.Configurations.Add(new ShipperMap());
+            modelBuilder.Configurations.Add(new SupplierMap());
+            modelBuilder.Configurations.Add(new TerritoryMap());
+        }
+    }
+}
