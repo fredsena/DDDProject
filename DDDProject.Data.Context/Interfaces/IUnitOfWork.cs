0a04ae3465adfc80927b0215b3f9c658d554ea71
fredsena <sena.fred@gmail.com>
2015-07-14 20:28:49 -0300

    more features

--- DDDProject.Data.Context/Interfaces/IUnitOfWork.cs
+++ DDDProject.Data.Context/Interfaces/IUnitOfWork.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace DDDProject.Data.Context.Interfaces
 {
-    interface IUnitOfWork
+    public interface IUnitOfWork<TContext>
+        where TContext : IDbContext, new()
     {
+        void BeginTransaction();
+        void SaveChanges();
     }
 }
